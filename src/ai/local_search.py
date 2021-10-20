1f83572191c36c04d065a813c86991a9347ab9af
giantandreas <giantandreas0311@gmail.com>
2021-10-20 16:29:09 +0700

    fixing bug in local search

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -17,12 +17,9 @@ class LocalSearch:
         self.thinking_time = time() + thinking_time
 
         state_value_now = self.state_value(state)
-        print("sebelum = " + str(state_value_now))
         selected_state = None
         selected_state_value = 0
         for new_state in self.state_generator(state,(state.round - 1) % 2 ):
-            print("kolom = " + str(new_state[1]))
-            print("shape = " + new_state[2])
             new_value = self.state_value(new_state[0])
             if( selected_state is None):
                 selected_state = new_state
@@ -37,35 +34,46 @@ class LocalSearch:
     def state_value(self, state: State):
         value_color = 0
         value_shape = 0
+
+        checked_point = set({})
         for titik in (self.baris_titik_tertinggi(state)):    # titik : offset 0 itu kolom dan 1 itu baris
+
             if(titik[1] >= state.board.row):    # ketika di awal
                 continue
 
             is_enemy_shape = state.board.board[titik[1]][titik[0]].shape == state.players[(state.round) % 2].shape 
             is_enemy_color = state.board.board[titik[1]][titik[0]].color == state.players[(state.round) % 2].color 
+
             if((not is_enemy_color) and (not is_enemy_shape)):
                 continue
             else:              
                 # untuk vertical
                 if(is_enemy_shape):
-                    print("masuk pertama shape")
-                    value_shape -= 2
+                    if((titik[1],titik[0],"s") not in checked_point):
+                            value_shape -= 2
+                            checked_point.add((titik[1],titik[0],"s"))
                 if(is_enemy_color):
-                    print("masuk pertama color")
-                    value_color -=1
+                    if((titik[1],titik[0],"c") not in checked_point):
+                            value_color -=1
+                            checked_point.add((titik[1],titik[0],"c"))
+
                 for j in range(state.board.row-titik[1]-1):
+                    temp_for_point = (titik[1] + j + 1, titik[0])
                     is_enemy_color = is_enemy_color and state.board.board[ titik[1] + j + 1 ][titik[0]].color == state.players[(state.round) % 2].color
                     is_enemy_shape = is_enemy_shape and state.board.board[ titik[1] + j + 1 ][titik[0]].shape == state.players[(state.round) % 2].shape
 
                     if(is_enemy_color):
-                        print("masuk sini color vertical")
-                        value_color -= 1
+                        if(temp_for_point + tuple("s") not in checked_point):
+                            value_color -= 1
+                            checked_point.add(temp_for_point + tuple("s"))
                     if(is_enemy_shape):
-                        print("masuk sini shape vertical")
-                        value_shape -= 2
+                        if(temp_for_point + tuple("c") not in checked_point):
+                            value_shape -= 2
+                            checked_point.add(temp_for_point + tuple("c"))
                     if((not is_enemy_color) and (not is_enemy_shape)):
                         break
 
+                
                 # untuk horizontal
                 kesamaan_warna_ditemukan = False
                 kesamaan_bentuk_ditemukan = False
@@ -78,9 +86,7 @@ class LocalSearch:
                 color_done = False
                 shape_done = False
                 for j in range (state.board.col):
-                    if(j == titik[1]):
-                        continue
-
+                    temp_for_point = (titik[1],j)
                     if(state.board.board[titik[1]][j].shape == state.players[(state.round) % 2].shape and not kesamaan_bentuk_ditemukan):
                         kesamaan_bentuk_ditemukan = True
                         titik_mulai_shape = j
@@ -88,8 +94,6 @@ class LocalSearch:
                         kesamaan_bentuk_ditemukan = False
                         titik_akhir_shape = j
                         if(titik[0] in range(titik_mulai_shape,titik_akhir_shape+1,1)):
-                            print("masuk sini shape horizontal")
-                            print("nilainya = " + str(shape_value_temp))
                             value_shape += shape_value_temp
                             shape_done = True
                     if(state.board.board[titik[1]][j].color == state.players[(state.round) % 2].color and not kesamaan_warna_ditemukan):
@@ -99,19 +103,22 @@ class LocalSearch:
                         kesamaan_warna_ditemukan = False
                         titik_akhir_color = j
                         if(titik[0] in range(titik_mulai_color,titik_akhir_color+1,1)):
-                            print("masuk sini color horizontal")
-                            print("nilainya = " + str(color_value_temp))
                             value_color += color_value_temp
-                            color_done = True
+                            color_done = True 
                     
                     if(shape_done and color_done):
                         break
                     
                     if(kesamaan_bentuk_ditemukan):
-                        
-                        shape_value_temp -= 2
+                        if(temp_for_point + tuple("s") not in checked_point):
+                            shape_value_temp -= 2
+                            checked_point.add(temp_for_point + tuple("s"))
                     if(kesamaan_warna_ditemukan):
-                        color_value_temp -=1
+                        if(temp_for_point + tuple("c") not in checked_point):
+                            color_value_temp -=1
+                            checked_point.add(temp_for_point + tuple("c"))
+
+                
                     
                 # untuk diagonal 1
                 kesamaan_warna_ditemukan = False
@@ -125,9 +132,7 @@ class LocalSearch:
                 color_done = False
                 shape_done = False
                 for titik_diagonal in self.diagonal_1(state.board.row,state.board.col,titik):
-                    if(titik_diagonal[1] == titik[1] and titik_diagonal[0] == titik[0]):
-                        continue
-                    print("titik diagonal 1: kol " + str(titik_diagonal[1]) + " row " + str(titik_diagonal[0]))
+                    temp_for_point = (titik_diagonal[1],titik_diagonal[0])
                     if(state.board.board[titik_diagonal[1]][titik_diagonal[0]].shape == state.players[(state.round) % 2].shape and not kesamaan_bentuk_ditemukan):
                         kesamaan_bentuk_ditemukan = True
                         titik_mulai_shape = titik_diagonal
@@ -135,9 +140,6 @@ class LocalSearch:
                         kesamaan_bentuk_ditemukan = False
                         titik_akhir_shape = titik_diagonal
                         if(titik[1] >= titik_mulai_shape[1] and titik[0] <= titik_akhir_shape[0]):
-                            print("masuk sini shape diagonal 1")
-                            print("nilainya = " + str(shape_value_temp))
-
                             value_shape += shape_value_temp
                             shape_done = True
                     if(state.board.board[titik_diagonal[1]][titik_diagonal[0]].color == state.players[(state.round) % 2].color and not kesamaan_warna_ditemukan):
@@ -147,9 +149,6 @@ class LocalSearch:
                         kesamaan_warna_ditemukan = False
                         titik_akhir_color = titik_diagonal
                         if(titik[1] >= titik_mulai_color[1] and titik[0] <= titik_akhir_color[0]):
-                            print("masuk sini color diagonal 1")
-                            print("nilainya = " + str(color_value_temp))
-
                             value_color += color_value_temp
                             color_done = True
                     
@@ -157,9 +156,13 @@ class LocalSearch:
                         break
 
                     if(kesamaan_bentuk_ditemukan):
-                        shape_value_temp -= 2
+                        if(temp_for_point + tuple("s") not in checked_point):
+                            shape_value_temp -= 2
+                            checked_point.add(temp_for_point + tuple("s"))
                     if(kesamaan_warna_ditemukan):
-                        color_value_temp -=1
+                        if(temp_for_point + tuple("c") not in checked_point):
+                            color_value_temp -=1
+                            checked_point.add(temp_for_point + tuple("c"))
 
                 # untuk diagonal 2
                 kesamaan_warna_ditemukan = False
@@ -173,9 +176,9 @@ class LocalSearch:
                 color_done = False
                 shape_done = False
                 for titik_diagonal in self.diagonal_2(state.board.row,state.board.col,titik): # disini ada masalah, shape lawan bakal di buat jadinya
+                    temp_for_point = (titik_diagonal[1],titik_diagonal[0])
                     if(titik_diagonal[1] == titik[1] and titik_diagonal[0] == titik[0]):
                         continue
-                    print("titik diagonal 2: kol " + str(titik_diagonal[1]) + " row " + str(titik_diagonal[0]))
                     if(state.board.board[titik_diagonal[1]][titik_diagonal[0]].shape == state.players[(state.round) % 2].shape and not kesamaan_bentuk_ditemukan and not shape_done):
                         kesamaan_bentuk_ditemukan = True
                         titik_mulai_shape = titik_diagonal
@@ -185,8 +188,6 @@ class LocalSearch:
                         is_in_column_range = titik[1] in range(titik_mulai_shape[1],titik_akhir_shape[1]+1,1) or titik[1] in range(titik_akhir_shape[1],titik_mulai_shape[1]-1,-1)
                         is_in_row_range = titik[0] in range(titik_mulai_shape[0],titik_akhir_shape[0]+1,1) or titik[0] in range(titik_akhir_shape[0],titik_mulai_shape[0]-1,-1)
                         if(is_in_column_range and is_in_row_range):
-                            print("masuk sini shape diagonal 2")
-                            print("nilainya = " + str(shape_value_temp))
                             value_shape += shape_value_temp
                             shape_done = True
                     if(state.board.board[titik_diagonal[1]][titik_diagonal[0]].color == state.players[(state.round) % 2].color and not kesamaan_warna_ditemukan and not color_done):
@@ -198,8 +199,6 @@ class LocalSearch:
                         is_in_column_range = titik[1] in range(titik_mulai_color[1],titik_akhir_color[1]+1,1) or titik[1] in range(titik_akhir_color[1],titik_mulai_color[1]-1,-1)
                         is_in_row_range = titik[0] in range(titik_mulai_color[0],titik_akhir_color[0]+1,1) or titik[0] in range(titik_akhir_color[0],titik_mulai_color[0]-1,-1)
                         if(is_in_column_range and is_in_row_range):
-                            print("masuk sini color diagonal 2")
-                            print("nilainya = " + str(color_value_temp))
                             value_color += color_value_temp
                             color_done = True
                     
@@ -207,22 +206,23 @@ class LocalSearch:
                         break
 
                     if(kesamaan_bentuk_ditemukan):
-                        shape_value_temp -= 2
+                        if(temp_for_point + tuple("s") not in checked_point):
+                            shape_value_temp -= 2
+                            checked_point.add(temp_for_point + tuple("s"))
                     if(kesamaan_warna_ditemukan):
-                        color_value_temp -=1
+                        if(temp_for_point + tuple("c") not in checked_point):
+                            color_value_temp -=1
+                            checked_point.add(temp_for_point + tuple("c"))
         nilai = value_color + value_shape
-        print("sesudah = " + str(nilai))
         return value_color + value_shape
 
     def baris_titik_tertinggi(self, state: State) -> List[Tuple[int,int]]:
         result = []
         for i in range(state.board.col):
-            baris = 0
             for j in range(state.board.row):
                 if(state.board.board[j][i].shape != "-" and state.board.board[j][i].color != "BLACK"):
+                    result.append([i,j])
                     break
-                baris = j  #perlu dipastikan baris ga balik nilai ke 0
-            result.append([i,baris+1]) #i kolom dan j baris
         return result
             
     def diagonal_1(self, row: int, column: int, position: Tuple[int,int]) -> List[Tuple[int,int]]:
